d0da55ee04f57161826d80213f53f232a90de4e2
zain0981p-maker <zain0981p@gmail.com>
2025-10-23 16:31:10 +0500

    uzs

---
+++ js.js
@@ -0,0 +1,17 @@
+const btm1=document.getElementById("btn")
+const serch=document.getElementById("serch")
+const rel=document.getElementById("result")
+const url="https://cbu.uz/uz/arkhiv-kursov-valyut/json/"
+//function
+async function zusulT1(){
+const data=await fetch(url)
+const javob=await data.json();
+const Text=serch.value.toUpperCase().trim()
+const top=javob.find(x=>x.Ccy===Text);
+const natija=`1 ${top.CcyNm_UZ} ~ ${top.Rate} so'm sana:${top.Date}`
+rel.textContent=natija
+}
+
+serch.addEventListener("input",()=>zusulT1())
+
+// btn.addEventListener("click",()=>zusulT1())
